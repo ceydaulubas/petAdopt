afac9747c4610aeaf72284579ff5a09f8fe99a0e
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-12-17 00:36:18 +0300

    fixed heroku part

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -22,3 +22,22 @@ const connectDb = (mongoUri) =>
 
 // Check if development environment is 
 NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS);
+
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_ATLAS_URI || MONGO_LOCAL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((x) => {
+    console.log(
+      `✅✅✅ Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
+  });
